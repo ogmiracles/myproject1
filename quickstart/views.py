5c82d41183a3c0cdc60065d1ad9caaa4a6f21d0a
Allen8Kane <allen8kane@gmail.com>
2023-03-15 23:04:29 +0600

    feat: create django project

---
+++ quickstart/views.py
@@ -0,0 +1,28 @@
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = {
+        'id': ['exact'],
+        'title': ['exact']
+    }
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+    filterset_fields = {
+        'id': ['exact'],
+        'price': ['exact', 'gt', 'lt', 'gte', 'lte'],
+        'title': ['exact'],
+        'category': ['exact']
+    }
+    ordering_fields = ['title', 'price']
+    ordering = ['title']
+    search_fields = ['title']
---
+++ tutorial/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework import routers
+from quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r'categories', views.CategoryViewSet)
+router.register(r'products', views.ProductViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+    path('admin/', admin.site.urls),
+]

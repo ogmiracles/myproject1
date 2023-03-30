a1631fdc240134550157053c054244c8adb637f0
Muhammed ogmiracles <km-030704@mail.ru>
2023-03-30 16:44:27 +0600

    sldd

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -115,7 +115,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 # Actual directory user files go to
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'mediafiles')
+MEDIA_ROOT = 'mediafiles/'
 
 # URL used to access the media
 MEDIA_URL = '/media/'
